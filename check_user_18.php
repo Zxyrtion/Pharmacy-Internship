9a272b47c8c64d4f9855e13f5be86fa067a2f7f2
jennyvieve <jennyvievemahinay@gmail.com>
2026-04-15 23:41:19 +0800

    Pushing process 9-18 to main

--- check_user_18.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'config.php';
-
-$result = $conn->query("SELECT id, first_name, last_name, role_name FROM users WHERE id = 18");
-$user = $result->fetch_assoc();
-
-echo json_encode($user, JSON_PRETTY_PRINT);
-?>
